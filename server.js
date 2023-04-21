e43c43e2282c19086b33d92084f87e4b378e9d03
Radhwan Muntasser Alzerary <48066813+Radwan-Alzerary@users.noreply.github.com>
2023-04-21 06:22:13 +0300

    .

--- server.js
+++ server.js
@@ -6,39 +6,41 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser());
 
+var clientResponseRef;
 app.get('/*', (req, res) => {
-    handleRequest(req, res);
-});
-
-app.post('/*', (req, res) => {
-    handleRequest(req, res);
-});
-
-function handleRequest(req, res) {
     var pathname = url.parse(req.url).pathname;
 
     var obj = {
         pathname: pathname,
-        method: req.method.toLowerCase(),
-        params: req.method === 'GET' ? req.query : req.body
-    };
+        method: "get",
+        params: req.query
+    }
 
     io.emit("page-request", obj);
-    io.on("page-response", createResponseHandler(res));
-}
+    clientResponseRef = res;
+})
 
-function createResponseHandler(res) {
-    return function(response) {
-        res.send(response);
-        io.removeAllListeners("page-response");
-    };
-}
+app.post('/*', (req, res) => {
+    var pathname = url.parse(req.url).pathname;
+
+    var obj = {
+        pathname: pathname,
+        method: "post",
+        params: req.body
+    }
+
+    io.emit("page-request", obj);
+    clientResponseRef = res;
+})
 
 io.on('connection', (socket) => {
     console.log('a node connected');
+    socket.on("page-response", (response) => {
+        clientResponseRef.send(response);
+    })
 })
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 http.listen(server_port, () => {
     console.log('listening on *:' + server_port);
-});
+})
